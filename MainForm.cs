860e630fac77602ceed9cba519d7feaefb601db3
Rishav Nath Pati <patirishavnath@gmail.com>
2024-01-26 00:49:21 +0530

    Refactor MainForm.cs and update .gitignore

--- MainForm.cs
+++ MainForm.cs
@@ -1,37 +1,66 @@
-using System.Diagnostics;
-using System.Runtime.InteropServices;
-using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace AppTimeTracker
 {
     public partial class MainForm : Form
     {
         private ApplicationTracker _tracker = new ApplicationTracker();
-        private ApplicationListView _listView;
+        private ApplicationListView _appListView;
         private Dictionary<string, ApplicationData> _appData = new Dictionary<string, ApplicationData>();
         private string _currentApp = "";
         private DateTime _lastSwitchTime;
         private System.Windows.Forms.Timer _timer;
+        private StatusStrip _statusStrip;
+        private ToolStripStatusLabel _statusLabel;
+        private ListView _listView; // ListView as a field
 
         public MainForm()
         {
             InitializeComponent();
+            InitializeCustomComponents();
+            SetUpTimer();
 
-            ListView listView = new ListView();
-            listView.Dock = DockStyle.Fill;
-            listView.View = View.Details;
-            listView.FullRowSelect = true;
-            listView.Columns.Add("Application", -2, HorizontalAlignment.Left);
-            listView.Columns.Add("Time Spent", -2, HorizontalAlignment.Left);
-            this.Controls.Add(listView);
+            _appListView = new ApplicationListView(_listView);
+            _lastSwitchTime = DateTime.Now;
+        }
 
-            _listView = new ApplicationListView(listView);
+        private void InitializeCustomComponents()
+        {
+            // ListView Setup
+            _listView = new ListView
+            {
+                Dock = DockStyle.Fill,
+                View = View.Details,
+                FullRowSelect = true
+            };
+            _listView.Columns.Add("Application", 200, HorizontalAlignment.Left);
+            _listView.Columns.Add("Time Spent", 150, HorizontalAlignment.Left);
+            _listView.ColumnWidthChanging += ListView_ColumnWidthChanging;
+            Controls.Add(_listView);
 
-            _timer = new System.Windows.Forms.Timer();
-            _timer.Interval = 1000; // Check every second
+            // Status Bar Setup
+            _statusStrip = new StatusStrip();
+            _statusLabel = new ToolStripStatusLabel { Text = "Ready" };
+            _statusStrip.Items.Add(_statusLabel);
+            Controls.Add(_statusStrip);
+
+            // Refresh Button Setup
+            Button refreshButton = new Button
+            {
+                Text = "Refresh",
+                Dock = DockStyle.Bottom
+            };
+            refreshButton.Click += RefreshButton_Click;
+            Controls.Add(refreshButton);
+        }
+
+        private void SetUpTimer()
+        {
+            _timer = new System.Windows.Forms.Timer { Interval = 1000 };
             _timer.Tick += Timer_Tick;
             _timer.Start();
-            _lastSwitchTime = DateTime.Now;
         }
 
         private void Timer_Tick(object? sender, EventArgs e)
@@ -54,7 +83,18 @@ namespace AppTimeTracker
                 _currentApp = activeApp;
             }
 
-            _listView.Update(_appData.Values.ToList());
+            _appListView.Update(_appData.Values.ToList());
+        }
+
+        private void RefreshButton_Click(object? sender, EventArgs e)
+        {
+            _appListView.Update(_appData.Values.ToList());
+            _statusLabel.Text = "List refreshed at " + DateTime.Now.ToString("T");
+        }
+
+        private void ListView_ColumnWidthChanging(object sender, ColumnWidthChangingEventArgs e)
+        {
+            e.Cancel = true; // Prevent column resize
         }
     }
 }
